a3dbf1523af1dd5d239a7fe2b8cc519e284e4282
Raizo-03 <ejaybuscato033@gmail.com>
2024-11-16 00:11:58 +0800

    add

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
+<?php
+// Get JawsDB connection information
+$jawsdb_url = parse_url(getenv("JAWSDB_URL"));
+$jawsdb_server = $jawsdb_url["host"];
+$jawsdb_username = $jawsdb_url["user"];
+$jawsdb_password = $jawsdb_url["pass"];
+$jawsdb_db = substr($jawsdb_url["path"], 1);
+
+// Connect to the database
+$conn = mysqli_connect($jawsdb_server, $jawsdb_username, $jawsdb_password, $jawsdb_db);
+
+// Check if the connection is successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully to JawsDB!";
+?>
+
+
 echo "hello"
 ?>
