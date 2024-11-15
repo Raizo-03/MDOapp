bf7b25461adc44f6a743903d89b57b12fafaa40d
Raizo-03 <ejaybuscato033@gmail.com>
2024-11-16 00:13:38 +0800

    add

--- index.php
+++ index.php
@@ -1,22 +1,18 @@
 <?php
-<?php
-// Get JawsDB connection information
-$jawsdb_url = parse_url(getenv("JAWSDB_URL"));
+// Get Heroku JawsDB connection information
+$jawsdb_url = parse_url(getenv("JAWSDB_URL")); // Use the JAWSDB_URL environment variable
 $jawsdb_server = $jawsdb_url["host"];
 $jawsdb_username = $jawsdb_url["user"];
 $jawsdb_password = $jawsdb_url["pass"];
-$jawsdb_db = substr($jawsdb_url["path"], 1);
+$jawsdb_db = substr($jawsdb_url["path"], 1); // Remove the leading '/' from the path
 
 // Connect to the database
 $conn = mysqli_connect($jawsdb_server, $jawsdb_username, $jawsdb_password, $jawsdb_db);
 
-// Check if the connection is successful
+// Check the connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
 echo "Connected successfully to JawsDB!";
-?>
-
-
-echo "hello"
 ?>
