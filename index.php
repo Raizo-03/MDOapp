f681f475d26773dfaac8dcc8ee01831f7006ed3b
Raizo-03 <ejaybuscato033@gmail.com>
2024-11-15 23:45:28 +0800

    Update PHP code to use JawsDB connection

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php
-// Get Heroku JawsDB connection information
-$jawsdb_url = parse_url(getenv("JAWSDB_URL")); // Use the JAWSDB_URL environment variable
+// Get JawsDB connection information
+$jawsdb_url = parse_url(getenv("JAWSDB_TEAL_URL"));
 $jawsdb_server = $jawsdb_url["host"];
 $jawsdb_username = $jawsdb_url["user"];
 $jawsdb_password = $jawsdb_url["pass"];
-$jawsdb_db = substr($jawsdb_url["path"], 1); // Remove the leading '/' from the path
+$jawsdb_db = substr($jawsdb_url["path"], 1);
 
 // Connect to the database
 $conn = mysqli_connect($jawsdb_server, $jawsdb_username, $jawsdb_password, $jawsdb_db);
 
-// Check the connection
+// Check if the connection is successful
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 echo "Connected successfully to JawsDB!";
 ?>
