958cf14d7b91e717e7c199ffe40e604d5d0c2c6d
Raizo-03 <ejaybuscato033@gmail.com>
2024-11-22 23:44:24 +0800

    updated db.php

--- db.php
+++ db.php
@@ -1,15 +1,18 @@
 <?php
-// db.php - Database connection file
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "MDOdb"; // Database name
+// Get Heroku JawsDB connection information
+$jawsdb_url = parse_url(getenv("JAWSDB_URL")); // Use the JAWSDB_URL environment variable
+$jawsdb_server = $jawsdb_url["host"];
+$jawsdb_username = $jawsdb_url["user"];
+$jawsdb_password = $jawsdb_url["pass"];
+$jawsdb_db = substr($jawsdb_url["path"], 1); // Remove the leading '/' from the path
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Connect to the database
+$conn = mysqli_connect($jawsdb_server, $jawsdb_username, $jawsdb_password, $jawsdb_db);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Check the connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
+echo "Connected successfully to JawsDB!";
 ?>
