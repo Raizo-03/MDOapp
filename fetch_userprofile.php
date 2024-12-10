8e756d43e577bd5581f88af33a664919e9bfc972
Raizo-03 <ejaybuscato033@gmail.com>
2024-12-11 06:40:44 +0800

    add

--- fetch_userprofile.php
+++ fetch_userprofile.php
@@ -1,6 +1,12 @@
 <?php
-// Include the database connection file
-include('db.php'); // This includes the JawsDB connection from db.php
+$jawsdb_url = parse_url(getenv("JAWSDB_URL")); // Use the JAWSDB_URL environment variable
+$jawsdb_server = $jawsdb_url["host"];
+$jawsdb_username = $jawsdb_url["user"];
+$jawsdb_password = $jawsdb_url["pass"];
+$jawsdb_db = substr($jawsdb_url["path"], 1); // Remove the leading '/' from the path
+
+// Connect to the database
+$conn = mysqli_connect($jawsdb_server, $jawsdb_username, $jawsdb_password, $jawsdb_db);
 
 // Get umak_email from the GET request
 if (isset($_GET['umak_email'])) {
--- update_userprofiledetails.php
+++ update_userprofiledetails.php
@@ -1,6 +1,12 @@
 <?php
-// Include the database connection file
-include('db.php'); // This includes the JawsDB connection from db.php
+$jawsdb_url = parse_url(getenv("JAWSDB_URL")); // Use the JAWSDB_URL environment variable
+$jawsdb_server = $jawsdb_url["host"];
+$jawsdb_username = $jawsdb_url["user"];
+$jawsdb_password = $jawsdb_url["pass"];
+$jawsdb_db = substr($jawsdb_url["path"], 1); // Remove the leading '/' from the path
+
+// Connect to the database
+$conn = mysqli_connect($jawsdb_server, $jawsdb_username, $jawsdb_password, $jawsdb_db);
 
 // Get POST data
 if (isset($_POST['umak_email']) && isset($_POST['contact_number']) && isset($_POST['address']) && isset($_POST['guardian_contact_number']) && isset($_POST['guardian_address'])) {
